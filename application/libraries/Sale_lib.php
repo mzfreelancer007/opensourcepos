f8f561c1d5970a7d274674a60366d7a946adaa53
objecttothis <objecttothis@gmail.com>
2020-07-02 10:46:20 +0400

    Bug fixes
    - Reverted some === to == unil unit tests could be written to confirm
    they do not break things.
    - Corrected value_exists function to properly check for date attributes
    - Removed unused variables.
    - Removed call to delete_orphaned_values because it is causing problems
    - Fixed code formatting
    - Removed trailing whitespace and whitespace on blank lines

--- application/libraries/Sale_lib.php
+++ application/libraries/Sale_lib.php
@@ -1278,7 +1278,6 @@ class Sale_lib
 	{
 		$cash_decimals = cash_decimals();
 		$cash_rounding_code = $this->CI->config->item('cash_rounding_code');
-		$rounded_total = $total;
 
 		return Rounding_mode::round_number($cash_rounding_code, $total, $cash_decimals);
 	}
